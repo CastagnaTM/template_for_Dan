25d997f0fa673172b5ccac627e35b53dabd6ecb6
castagnaTM <castagna.tm@gmail.com>
2020-03-09 21:07:57 -0400

    began basic section styling

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -8,12 +8,19 @@ export default class Image extends React.Component {
         showText: false
     }
     componentDidMount() {
-        window.addEventListener('resize', () => {
-            this.setState({
-                isMobile: window.innerWidth < 800
-            });
-        }, false);
+        window.addEventListener('resize', this.resizeListener);
     }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.resizeListener);
+    }
+
+    resizeListener = () => {
+        this.setState({
+            isMobile: window.innerWidth < 800
+        });
+    }
+
 
     revealText = () => {
         this.setState({
@@ -24,7 +31,7 @@ export default class Image extends React.Component {
     render(){
         if(this.state.isMobile){
             return (
-                <div className='component-body'>
+                <div className='component-body' id={this.props.item.id % 2 === 0 ? 'designs-b' : 'designs-a'}>
                     <div className='component-image-div' onClick={() => this.revealText()}>
                         <figure>
                             <img src={this.props.item.name} className='component-image' alt='design images'></img>
@@ -42,7 +49,7 @@ export default class Image extends React.Component {
         else {
             if(this.props.item.id % 2 === 0){
                 return (
-                    <div className='component-body'>
+                    <div className='component-body' id='designs-b'>
                         <div className='component-text' style={{display: this.state.showText ? 'block' : 'none' }}>
                             <p>
                                 Proident nostrud reprehenderit aliqua sunt laboris nostrud velit ex commodo eiusmod fugiat aliqua esse eu. Id laborum in irure ex duis voluptate aliquip dolore nostrud aliquip Lorem. Irure quis dolore nulla voluptate dolore sit adipisicing. Dolore voluptate mollit velit consectetur dolor cillum adipisicing irure incididunt et sunt.
@@ -59,7 +66,7 @@ export default class Image extends React.Component {
             }
             else {
                 return (
-                    <div className='component-body'>
+                    <div className='component-body' id='designs-a'>
                         <div className='component-image-div' onClick={() => this.revealText()}>
                             <figure>
                                 <img src={this.props.item.name} className='component-image' alt='design images'></img>
