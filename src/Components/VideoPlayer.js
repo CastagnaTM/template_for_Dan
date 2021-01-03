6e85afae4b7bc0e2a4ba7f2c1570973c6b795062
castagnaTM <castagna.tm@gmail.com>
2021-01-03 11:17:12 -0500

    set up daily motion params

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -6,7 +6,7 @@ export default function VideoPlayer() {
 
     return (
         <ReactPlayer 
-            url='https://www.dailymotion.com/video/x7yg8h9'
+            url='https://www.dailymotion.com/video/x7ygcbd'
             className='react-player'
             width='100%'
             height='100%'
@@ -14,6 +14,14 @@ export default function VideoPlayer() {
             playing={true}
             muted={true}
             style={{display: 'inline-block', padding: window.innerWidth > 1200 ? '8%' : '0'}}
+            config={{
+                dailymotion: {
+                    params: {
+                        "queue-autoplay-next": false,
+                        "queue-enable": false,
+                    }
+                }
+            }}
             />
     )
 }
